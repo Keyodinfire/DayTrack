92eac2ffc02ff6df8637d6641f7ecd788a4e28da
Andy Molla <Andy.Molla@perficient.com>
2022-06-13 15:39:49 -0500

    Setup

---
+++ techbootcamp-AndyMolla/src/main/java/com/perficient/techbootcampAndyMolla/TechbootcampAndyMollaApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampAndyMolla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampAndyMollaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampAndyMollaApplication.class, args);
+	}
+
+}
